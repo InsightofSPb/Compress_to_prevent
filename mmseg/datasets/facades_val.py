a4163881602674e034d1cdd8ff67a6a65d2f2bcb
Alexander Topolnitskii <123558403+InsightofSPb@users.noreply.github.com>
2025-12-07 16:14:25 +0300

    Fix facades dataset pipelines for preprocessed inputs

--- mmseg/datasets/facades_val.py
+++ mmseg/datasets/facades_val.py
@@ -1,11 +1,16 @@
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
-    train=dict(
+    val=dict(
         type="CustomDataset",
         data_root="/path/to/data/",
         img_dir="/home/sasha/LPOSS/data_augmented/facades_aug/data_prepared/val/images",
         ann_dir="/home/sasha/LPOSS/data_augmented/facades_aug/data_prepared/val/masks",
-        pipeline=[...],
+        pipeline=[
+            dict(type="LoadImageFromFile"),
+            dict(type="LoadAnnotations"),
+            dict(type="DefaultFormatBundle"),
+            dict(type="Collect", keys=["img", "gt_semantic_seg"]),
+        ],
     ),
 )
