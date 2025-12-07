ba1dd272641020b53f440a7dd70a8ed09d04637c
Alexander <taa13102016@gmail.com>
2025-12-07 16:01:36 +0300

    add train, val finetune configs

---
+++ mmseg/datasets/facades_val.py
@@ -0,0 +1,11 @@
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type="CustomDataset",
+        data_root="/path/to/data/",
+        img_dir="/home/sasha/LPOSS/data_augmented/facades_aug/data_prepared/val/images",
+        ann_dir="/home/sasha/LPOSS/data_augmented/facades_aug/data_prepared/val/masks",
+        pipeline=[...],
+    ),
+)
