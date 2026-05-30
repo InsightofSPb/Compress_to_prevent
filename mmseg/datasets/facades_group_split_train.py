ff23cb1a257923852bf72a9bf9b0edbad418c668
Alexander Topolnitskii <123558403+InsightofSPb@users.noreply.github.com>
2026-05-30 19:07:00 +0300

    Add training config for prepared facade group split tiles

---
+++ mmseg/datasets/facades_group_split_train.py
@@ -0,0 +1,61 @@
+import os
+
+classes = (
+    "BACKGROUND",
+    "CRACK",
+    "SPALLING",
+    "DELAMINATION",
+    "MISSING_ELEMENT",
+    "WATER_STAIN",
+    "EFFLORESCENCE",
+    "CORROSION",
+    "ORNAMENT_INTACT",
+    "REPAIRS",
+    "TEXT_OR_IMAGES",
+)
+
+palette = [
+    [0, 0, 0],
+    [229, 57, 53],
+    [30, 136, 229],
+    [67, 160, 71],
+    [251, 140, 0],
+    [142, 36, 170],
+    [253, 216, 53],
+    [0, 172, 193],
+    [158, 158, 158],
+    [78, 158, 158],
+    [142, 126, 71],
+]
+
+train_root = os.environ.get(
+    "FACADES_SEG_TRAIN_ROOT",
+    "data/facades_group_split/segmentation_train_tiles",
+)
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type="CustomDataset",
+        data_root=train_root,
+        img_dir="images",
+        ann_dir="masks",
+        img_suffix=".png",
+        seg_map_suffix=".png",
+        ignore_index=255,
+        reduce_zero_label=False,
+        classes=classes,
+        palette=palette,
+        pipeline=[
+            dict(type="LoadImageFromFile"),
+            dict(type="LoadAnnotations"),
+            dict(type="DefaultFormatBundle"),
+            dict(
+                type="Collect",
+                keys=["img", "gt_semantic_seg"],
+                meta_keys=("filename", "ori_filename", "ori_shape", "img_shape", "pad_shape"),
+            ),
+        ],
+    ),
+)
